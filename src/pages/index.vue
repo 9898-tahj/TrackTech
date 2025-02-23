9cab1ca8f094d141d12dc5cd5e8963aaf344a643
9898-tahj <tahjaywalker22@gmail.com>
2025-02-23 17:34:34 -0500

    Changes to landing page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <section class="h-screen w-full flex justify-center items-center bg-green-500 text-white">
-        <h1 class="text-4xl">AuthPage</h1>
+    <section class="h-screen w-full flex justify-center items-center bg-indigo-500 text-white">
+        <h1 class="text-6xl">Wellcome To TrackTech</h1>
     </section>
 </template>
